96bf0c696a0cb23960f9f69dfc79b7e6525a31b5
Antonio Marusic <antonio.marusic94@gmail.com>
2020-09-07 18:45:47 +0200

    Added ability to add, remove books, mark as read and generate card based on input

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const form = document.querySelector("form");
-const cardSection = document.querySelector(".card-section");
+const form = document.querySelector('form');
+const cardSection = document.querySelector('.card-section');
+const container = document.querySelector('.container');
+const newBook = document.querySelector('.new-book');
 const myLibrary = [];
 
 function Book(author, title, pages, image, read) {
@@ -9,14 +11,22 @@ function Book(author, title, pages, image, read) {
   this.image = image;
   this.read = read;
 }
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   addBookToLibrary(e);
+  updateCardsSection();
   form.reset();
-  form.classList.toggle("hidden");
-  cardSection.classList.toggle("hidden");
-  generateBookCard();
+  form.classList.toggle('hidden');
+  container.classList.toggle('hidden');
+});
+
+newBook.addEventListener('click', () => {
+  form.classList.toggle('hidden');
+  container.classList.toggle('hidden');
 });
 
 function addBookToLibrary() {
@@ -28,41 +38,57 @@ function addBookToLibrary() {
     form.read.checked
   );
   myLibrary.push(newBook);
-  updateCardsSection();
 }
 
 function updateCardsSection() {
+  cardSection.innerHTML = '';
   myLibrary.forEach((el, i) => {
-    generateBookCard(el, i);
+    let card = document.createElement('div');
+    card.classList.add('book-card');
+    
+    if (el.image) {
+      card.innerHTML += `<div class="book-image">
+                          <img src="${el.image}" alt="">
+                        </div>`;
+    }
+    card.innerHTML += `<div class="book-information">
+                          <div class="information-section">
+                              <p class="information">Author:</p>
+                              <p class="author">${el.author}</p>
+                          </div>
+                          <div class="information-section">
+                              <p class="information">Title:</p>
+                              <p class="title">${el.title}</p>
+                          </div>`;
+    if (el.pages) {
+      card.querySelector('.book-information').innerHTML += `<div class="information-section">
+                              <p class="information">Length:</p>
+                              <p class="length">${el.pages}</p>
+                          </div>`;
+    }
+
+    card.querySelector('.book-information').innerHTML += `<div class="information-section">
+                              <p class="information">Read:</p>
+                              <p class="length">${el.read ? 'Yes' : 'No'}</p>
+                          </div>
+                      </div>
+                      <div class="book-card-bottom">
+                          <button class="remove-book" data-cardid="${i}">Delete</button>
+                          <button class="mark-read" data-cardid="${i}">Mark as read</button>
+                      </div>`;
+
+    cardSection.appendChild(card);
+    card.querySelector('.remove-book').addEventListener('click', deleteBook);
+    card.querySelector('.mark-read').addEventListener('click', markAsRead);
   });
 }
-function generateBookCard(el) {
-  let card = document.createElement("div");
-  card.classList.add("book-card");
-  card.innerHTML = `<div class="book-image">
-                <img src="${el.image}" alt="">
-            </div>
-            <div class="book-information">
-                <div class="information-section">
-                    <p class="information">Author:</p>
-                    <p class="author">${el.image}</p>
-                </div>
-                <div class="information-section">
-                    <p class="information">Title:</p>
-                    <p class="title">${el.title}</p>
-                </div>
-                <div class="information-section">
-                    <p class="information">Length:</p>
-                    <p class="length">${el.pages}</p>
-                </div>
-                <div class="information-section">
-                    <p class="information">Read:</p>
-                    <p class="length">${el.read ? "Yes" : "No"}</p>
-                </div>
-            </div>
-            <div class="book-card-bottom">
-                <button class="remove-book">Delete</button>
-                <button class="mark-read">Mark as read</button>
-            </div>`;
-  cardSection.appendChild(card);
+
+function markAsRead(e) {
+  myLibrary[e.target.dataset.cardid].toggleRead();
+  updateCardsSection();
+}
+
+function deleteBook(e) {
+  myLibrary.splice(e.target.dataset.cardid, 1);
+  updateCardsSection();
 }
